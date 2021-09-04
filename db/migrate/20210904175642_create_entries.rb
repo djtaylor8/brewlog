6d4edb6798dd8b06772442210581c21589cd5593
DJ Taylor <dtaylor88@gmail.com>
2021-09-04 11:08:01 -0700

    add user and entry models with relationships

---
+++ db/migrate/20210904175642_create_entries.rb
@@ -0,0 +1,13 @@
+class CreateEntries < ActiveRecord::Migration[6.1]
+  def change
+    create_table :entries do |t|
+      t.text :name
+      t.text :location
+      t.text :beers
+      t.text :notes
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
