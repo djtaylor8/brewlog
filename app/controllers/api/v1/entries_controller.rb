80a6cbb2c9f5cdbc91bf43fbcc9f14319922804f
DJ Taylor <dtaylor88@gmail.com>
2021-09-08 15:55:01 -0700

    add update/destroy to entry controller

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,13 +1,43 @@
 class Api::V1::EntriesController < ApplicationController
+    before_action :set_task, only: [:update, :destroy]
     
     def index
       entries = Entry.all 
       render json: entries 
     end
 
+    def create
+      entry = Entry.new(entry_params)
+      if entry.save
+        render json: entry 
+      else
+        render json: { status: 'error', message: "Sorry, the entry did not save!"}
+      end
+    end
+
+    def update
+      if @entry.update(entry_params)
+        render json: @entry
+      else 
+        render json:  { status: 'error', message: 'Sorry, the entry was not updated!'}
+      end
+    end
+
+    def destroy
+      if @entry.delete
+        render json: @entry
+      else
+        render json: { status: 'error', message: 'Sorry, the entry was not deleted!'}
+      end
+    end
+
     private 
 
     def entry_params
       params.require(:entry).permit(:name, :location, :beers, :notes, :user_id)
+    end
+
+    def set_entry
+      @entry = Entry.find(params[:id])
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
+lou = User.create(name: 'Lou')
 georgia = User.create(name: 'Georgia')
 ozzy = User.create(name: 'Ozzy')
 ted = User.create(name: 'Ted')
 
-Entry.create(name: "golden road", location: "dtla", beers: 'mango cart', notes: "nice fruit beers", user: georgia)
-Entry.create(name: "brouwerij west", location: "san pedro", beers: 'picnic lightning', notes: "amazing hazy beers", user: ozzy)
-Entry.create(name: "shiner", location: "shiner, tx", beers: 'shiner bock', notes: "classic brew", user: ted)
+Entry.create(name: "highland park brewery", location: "1220 N Spring St, Los Angeles, CA", user: lou)
+Entry.create(name: "macleods", location: "Van Nuys, CA", user: lou)
+Entry.create(name: "paperback", location: "Glendale, CA", user: lou)
+Entry.create(name: "golden road", location: "Los Angeles, CA", beers: 'mango cart', notes: "nice fruit beers", user: georgia)
+Entry.create(name: "brouwerij west", location: "San Pedro, CA", beers: 'picnic lightning', notes: "amazing hazy beers", user: ozzy)
+Entry.create(name: "shiner", location: "Shiner, TX", beers: 'shiner bock', notes: "classic brew", user: ted)
 
